41022328f897b47a011e3432bc4d28e97bd73120
irwan alfaz <alfazidtech@gmail.com>
2020-07-15 18:56:34 +0700

    update

--- src/main/java/com/alfazid/siasd/controller/MapelController.java
+++ src/main/java/com/alfazid/siasd/controller/MapelController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.xml.ws.http.HTTPException;
 import java.util.Optional;
 
 /**
